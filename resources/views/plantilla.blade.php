6ec75a4e914b0b6c419b12a4cbb66447200ce567
htqa123 <harold_uruena@yahoo.com>
2020-10-20 15:35:10 -0500

    creando las vistas

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <nav>
+        <ul>
+            <li><a href="/"></a>welcome</a></li>
+            <li><a href="/about"></a>home</a></li>
+            <li><a href="/contac"></a>contac</a></li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
